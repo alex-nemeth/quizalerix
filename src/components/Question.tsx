bb62251f5f2d2f17baea5c31539ed9017fd4f0df
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-04-08 23:42:19 +0200

    wip

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,16 +1,51 @@
 import { useState, useEffect } from "react";
 
 export default function Question(props: any) {
-  const [question] = useState(props.data);
+    const [question, setQuestion] = useState(props.data);
+    const [answers, setAnswers] = useState([""]);
+    const [selected, setSelected] = useState("default");
 
-  return (
-    <div>
-      <p>{question.question}</p>
-      <p>{question.category}</p>
-      <p>{question.type}</p>
-      <p>{question.difficulty}</p>
-      <p>{question.correct_answer}</p>
-      <p>{question.incorrect_answers}</p>
-    </div>
-  );
+    useEffect(() => {
+        let answersArr = [];
+        answersArr.push(question.correct_answer);
+        question.incorrect_answers.map((answer: any) => {
+            answersArr.push(answer);
+        });
+        setAnswers(answersArr);
+        console.log(answers);
+    }, []);
+
+    function selectAnswer(e: any) {
+        setSelected(e.target.value);
+    }
+
+    function renderOptions() {
+        return answers.map((answer: any) => {
+            return (
+                <>
+                    <p>{answer}</p>
+                    <input
+                        type="radio"
+                        name={question.question}
+                        checked={selected !== ""}
+                        onChange={selectAnswer}
+                        value={answer}
+                    ></input>
+                </>
+            );
+        });
+    }
+
+    return (
+        <>
+            {question.question}
+            {renderOptions()}
+        </>
+    );
 }
+//  <p>{question.question}</p>
+//  <p>{question.category}</p>
+//  <p>{question.type}</p>
+//  <p>{question.difficulty}</p>
+//  <p>{question.correct_answer}</p>
+//  <p>{question.incorrect_answers}</p>
--- src/components/Questions.tsx
+++ src/components/Questions.tsx
@@ -3,26 +3,40 @@ import OpenTriviaApi from "../api/OpenTriviaApi";
 import Question from "./Question";
 
 export default function Questions(props: any) {
-  const openTriviaApi = new OpenTriviaApi();
+    const openTriviaApi = new OpenTriviaApi();
 
-  const [state, setState] = useState({ questions: [], params: props.params });
+    const [state, setState] = useState({ questions: [], params: props.params });
+    const [selected, setSelected] = useState([{}]);
 
-  useEffect(() => {
-    fetch(openTriviaApi.constructQuestionsUrl(state.params))
-      .then((response) => response.json())
-      .then((data) => setState({ ...state, questions: data.results }));
+    useEffect(() => {
+        fetch(openTriviaApi.constructQuestionsUrl(state.params))
+            .then((response) => response.json())
+            .then((data) => setState({ ...state, questions: data.results }));
 
-    console.log(openTriviaApi.constructQuestionsUrl(state.params));
-    console.log(state.questions);
-  }, []);
+        console.log(openTriviaApi.constructQuestionsUrl(state.params));
+        console.log(state.questions);
+    }, []);
 
-  function displayQuestions(): JSX.Element[] {
-    return state.questions.map((question) => (
-      <div>
-        <Question data={question} />
-      </div>
-    ));
-  }
+    function selectOption() {
+        setSelected((prevSelected) => {
+            return {
+                ...prevSelected,
+            };
+        });
+    }
 
-  return <div>{displayQuestions()};</div>;
+    function displayQuestions(): JSX.Element[] {
+        return state.questions.map((question) => (
+            <div>
+                <Question select={selectOption} data={question} />
+            </div>
+        ));
+    }
+
+    return (
+        <div>
+            {displayQuestions()}
+            <button onClick={() => props.submit}>Submit</button>
+        </div>
+    );
 }
