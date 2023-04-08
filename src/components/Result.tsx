017b2cd0bc8818f6f0ff28d318d463776aaf28cb
eSarmir <e.sarmir@gmail.com>
2023-04-08 22:01:35 +0200

    Adds navigation button and which navigates to Selection

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,36 +1,45 @@
 import { useState, useEffect } from "react";
 
 export default function Result(props: any) {
-	const [answers, setAnswers] = useState([{ correct: "", selected: "" }]);
-	const [numberOfCorrectAnswers, setNumberOfCorrectAnswers] = useState(0);
-	const [totalNumberOfAnswers] = useState(props.answers.length);
+    const [answers, setAnswers] = useState([{ correct: "", selected: "" }]);
+    const [numberOfCorrectAnswers, setNumberOfCorrectAnswers] = useState(0);
+    const [totalNumberOfAnswers] = useState(props.answers.length);
 
-	useEffect(() => {
-		setAnswers(props.answers);
+    useEffect(() => {
+        setAnswers(props.answers);
 
-		evaluateAnswers();
-	});
+        evaluateAnswers();
+    });
 
-	function evaluateAnswers(): void {
-		let countCorrectAnswers: number = 0;
+    function evaluateAnswers(): void {
+        let countCorrectAnswers: number = 0;
 
-		answers.forEach((answer) => {
-			if (isCorrect(answer.correct, answer.selected)) {
-				setNumberOfCorrectAnswers(++countCorrectAnswers);
-			}
-		});
-	}
+        answers.forEach((answer) => {
+            if (isCorrect(answer.correct, answer.selected)) {
+                setNumberOfCorrectAnswers(++countCorrectAnswers);
+            }
+        });
+    }
 
-	function isCorrect(correctAnswer: string, selectedAnswer: string): boolean {
-		return correctAnswer === selectedAnswer;
-	}
+    function isCorrect(correctAnswer: string, selectedAnswer: string): boolean {
+        return correctAnswer === selectedAnswer;
+    }
 
-	return (
-		<div>
-			<p>
-				Number of corect answers {numberOfCorrectAnswers} out of{" "}
-				{totalNumberOfAnswers}
-			</p>
-		</div>
-	);
+    return (
+        <div>
+            <div>
+                <p>
+                    Number of corect answers {numberOfCorrectAnswers} out of{" "}
+                    {totalNumberOfAnswers}
+                </p>
+            </div>
+
+            <button
+                onClick={() => props.navigate(1)}
+                className="selection--start-btn"
+            >
+                New quiz
+            </button>
+        </div>
+    );
 }
