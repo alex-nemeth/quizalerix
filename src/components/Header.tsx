1705d6c0c7cda31a7b1a1757193321fb5fa0cb71
eSarmir <e.sarmir@gmail.com>
2023-04-11 17:52:47 +0200

    Adds quizalerix favicon and logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
-import react from "react";
+import logo from "../images/quizalerix-logo.png";
 import { Link } from "react-router-dom";
 
 export default function Header() {
     return (
-        <div className="container-fluid text-center">
-            <h1 className="row-1">
-                <Link to="/">Quizalerix</Link>
-            </h1>
+        <div className="container-fluid text-center mb-4">
+            <Link to="/">
+                <img className="img-fluid" width={512} src={logo}></img>
+            </Link>
         </div>
     );
 }
