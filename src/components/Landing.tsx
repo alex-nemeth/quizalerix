1ed420e421d122ac0f5b0a23c8a7e9653a6fcb89
Alex Németh <116309918+alex-nemeth@users.noreply.github.com>
2023-04-12 18:59:16 +0200

    styled landing (#49)

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,18 +1,16 @@
 import react, { useState } from "react";
 import { Link } from "react-router-dom";
-import Button from "react-bootstrap/Button";
 
 export default function Landing() {
     return (
-        <div className="d-flex flex-column align-items-center">
-            <p>Welcome to Quizalerix!</p>
-            <p>
-                Our trivia app that alows you to choose from various categories
-                fueled by API of OpenTrivia.
-            </p>
-            <p>Click the button below to set up your quiz!</p>
+        <div className="d-flex flex-column align-items-center justify-content-center">
             <Link to="/selection">
-                <Button variant="purple">Start</Button>
+                <button
+                    type="button"
+                    className="btn btn-outline-lightblue px-4 my-3"
+                >
+                    Create a quiz
+                </button>
             </Link>
         </div>
     );
